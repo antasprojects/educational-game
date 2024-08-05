ba5869fc856e20db7b1e51593c83090c009fa372
Antoni Szczekot <asatki@gmail.com>
2024-08-05 16:11:48 +0100

    add paths for register and login

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 
-// const usersController = require('../controllers/users.js');
+const usersController = require('../controllers/users.js');
 
 const usersRouter = Router();
 
-// usersRouter.post("/register", usersController.register);
-// usersRouter.post("/login", usersController.login);
+usersRouter.post("/register", usersController.register);
+usersRouter.post("/login", usersController.login);
 
 module.exports = usersRouter;
