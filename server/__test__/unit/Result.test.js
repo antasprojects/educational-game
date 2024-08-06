898e527a04bccc4d29702a4713e20ba1ead80c22
Mohamed <mmuseaden@gmail.com>
2024-08-06 11:14:20 +0100

    Incorporate testing scripts into package.json

--- server/__test__/unit/Result.test.js
+++ server/__test__/unit/Result.test.js
@@ -4,6 +4,54 @@ const Result = require("../../models/Result");
 
 describe("Result Model", () => {
     describe("getAll", () => {
-        
+        it("should return list of all results", async () => {
+            // Arrange
+            const datenow = new Date();
+            const mockResults = [
+                {
+                    id: 1,
+                    user_id: 1,
+                    score: 6,
+                    question_id: 3,
+                    created_at: datenow,
+                    updated_at: datenow,
+                },
+                {
+                    id: 2,
+                    user_id: 3,
+                    score: 8,
+                    question_id: 1,
+                    created_at: datenow,
+                    updated_at: datenow,
+                },
+                {
+                    id: 3,
+                    user_id: 5,
+                    score: 10,
+                    question_id: 2,
+                    created_at: datenow,
+                    updated_at: datenow,
+                },
+            ];
+
+            jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockResults });
+
+
+            // Act
+            const users = await Result.getAll();
+
+            // Assert
+            expect(Result).toBeDefined();
+            expect(Result.getAll).toBeDefined();
+            expect(db.query).toHaveBeenCalledTimes(1);
+            expect(users[2].user_id).toBe(3);
+            expect(users.every(user => user instanceof Result)).toBe(true);
+        });
+
+        it("throws an error if no users are found", async () => {
+            jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
+
+            await expect(Result.getAll()).rejects.toThrow("No users available")
+        })
     });
 });
