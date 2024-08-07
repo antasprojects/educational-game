045316d26ca2ce5b393d784120c04c1b4c7dda9c
Antoni Szczekot <asatki@gmail.com>
2024-08-07 16:34:01 +0100

    fix bug

--- server/__test__/unit/models/User.test.js
+++ server/__test__/unit/models/User.test.js
@@ -3,15 +3,81 @@ const db = require('../../../db/connect')
 
 
 
-// describe('User', () => {
-//     beforeEach(() => jest.clearAllMocks())
+describe('User model', () => {
+    beforeEach(() => jest.clearAllMocks())
 
-//     afterAll(() => jest.resetAllMocks())
+    afterAll(() => jest.resetAllMocks())
 
-//     describe("getUserById", () => {
-//         it ("should return user", async () => {
-//             jst.spyOn(db, 'query')
-//             .mockResolvedValueOnce
-//         })
-//     }
-// })
+    describe("getUserById", () => {
+        it ("should return user", async () => {
+            jest.spyOn(db, 'query')
+            .mockResolvedValueOnce({
+                rows: [{id : 1, email: 'antek@email.com', password: "xd123", admin: false}]
+            })
+
+            const user = await User.getUserById(1)
+            expect(typeof user).toBe('object')
+            expect(user.email).toBe("antek@email.com")
+            expect(user.password).toBe("xd123")
+
+        })
+
+        it('should throw an Error on db query error', async () => {
+
+            jest.spyOn(db, 'query')
+              .mockResolvedValueOnce({ rows: [] })
+
+            try {
+                await User.getUserById()
+            } catch (err) {
+                expect(err).toBeDefined()
+                expect(err.message).toBe("Unable to locate user.")
+            }
+
+        })
+
+    })
+
+    describe("getUserByEmail", () => {
+
+        it ("should return user", async () => {
+            jest.spyOn(db, 'query')
+            .mockResolvedValueOnce({
+                rows: [{id : 1, email: 'antek@email.com', password: "xd123", admin: false}]
+            })
+
+            const user = await User.getUserByEmail("antek@email.com")
+            console.log(user);
+            expect(typeof user).toBe('object')
+            expect(user.id).toBe(1)
+            expect(user.password).toBe("xd123")
+
+        })
+
+        it('should throw an Error on db query error', async () => {
+
+            jest.spyOn(db, 'query')
+              .mockResolvedValueOnce({ rows: [] })
+
+            try {
+                await User.getUserById()
+            } catch (err) {
+                expect(err).toBeDefined()
+                expect(err.message).toBe("Unable to locate user.")
+            }
+
+        })
+    })
+
+    describe("create", () => {
+        it('resolves with successful creation', async () => {
+        
+            const user = await User.create({email: 'antek@email.com', password: "xd1234"})
+            console.log(user);
+
+            expect(country instanceof Country).toBe(true)
+
+        })
+    })
+
+})
--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,7 @@ class User {
         this.id = id;
         this.email = email;
         this.password = password;
+        this.admin = false
     }
 
     static async getUserById(id) {
