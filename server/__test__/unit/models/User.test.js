ee81b838a74ae87bbe440597338d7031542ab264
Antoni Szczekot <asatki@gmail.com>
2024-08-07 14:56:59 +0100

    User test

---
+++ server/__test__/unit/models/User.test.js
@@ -0,0 +1,19 @@
+const User = require('../../../models/User')
+const db = require('../../../db/connect')
+
+
+
+describe('User', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+
+    describe("getUserById", () => {
+        it ("should return user", async () => {
+            jst.spyOn(db, 'query')
+            .mockResolvedValueOnce({
+                rows: [('john_doe', 'password123', 'john@email.com', FALSE)]
+            })
+        })
+    }
+})
